04a1dadeed74a3ab9b949f61d06473db314afddc
mohamed younes <bettiouim77@gmail.com>
2023-09-09 08:38:58 +0100

    improved it

--- app/components/HeroBanner.jsx
+++ app/components/HeroBanner.jsx
@@ -3,18 +3,36 @@ import Image from 'next/image'
 import React from 'react'
 import { urlFor } from '@/lib/client'
 import Link from 'next/link'
+import { getPlaiceholder } from "plaiceholder";
+ 
 
 
-const HeroBanner = ({banner}) => {
+
+const HeroBanner = async ({banner}) => {
+  let base
+  try {
+    const src = urlFor(banner.image).url();
+   
+    const buffer = await fetch(src).then(async (res) =>
+      Buffer.from(await res.arrayBuffer())
+    );
+   
+    const { base64 } = await getPlaiceholder(buffer);
+   base = base64
     
-    
+  } catch (err) {
+    err;
+  }
+ 
   return (
     <div className=' hero-banner-container'>
         <div>
             <p className="beats-solo">{banner.smallText}</p>
             <h3 className=' font-bold'>{banner.midText} </h3>
             <h1  className=' font-bold max-lg:!text-[8em] max-sm:!text-[3em] max-md:!text-[5em] '>{banner.largeText1} </h1>
-            <Image src={urlFor(banner.image).url()} width={400} height={400} alt='headphone' className='
+            <Image placeholder='blur'
+            blurDataURL={base}
+            priority={false} src={urlFor(banner.image).url()} width={400} height={400} alt='headphone' className='
             max-sm:w-56 max-md:h-56 max-md:-top-10
             max-lg:w-72 max-lg:h-72 max-lg:-top-10 max-lg:right-0 hero-banner-image'/>
             <Link href={`/product/${banner.product}`} className="btn max-md:text-lg mx-auto text-center block  !w-fit ">{banner.buttonText} </Link>
